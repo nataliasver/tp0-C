91296717fda9a34fd7c38d940bf2a4a6f4794eb0
nataliasver <brujinat@gmail.com>
2015-04-10 22:25:57 -0300

    Se agrega el .c de hello world

---
+++ hello.c
@@ -0,0 +1,12 @@
+// Nombre: Hola mundo!
+// Alumna: Natalia S. Verdura
+// Fecha de última modificación: 10-4-2015
+
+
+#include <stdio.h>
+
+int main ()
+{
+    printf ("Hello world! \n");
+    return 0;
+}
